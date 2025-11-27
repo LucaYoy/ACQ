e321b7003cffb5cf5bc8242cfe01889c2dec39f3
Andreu Anglés Castillo <andreu.cs.95@gmail.com>
2025-11-27 11:26:40 +0100

    Added run scripts

---
+++ run_scripts/ACQ_run_ord.py
@@ -0,0 +1,75 @@
+import Hamiltonian as ham
+import scipy.sparse as sp
+import numpy as np
+from matplotlib import pyplot as plt
+import Evolution_sim as evol
+import scipy.io as sio
+
+def fidelity_pure(psi,phi):
+    '''
+    input values should be column vectors
+    '''
+    F=np.abs(psi.conj().T@phi)**2
+    return F[0,0]
+
+def variance(psi,H):
+    V=np.real((psi.conj().T@H@H@psi)-(psi.conj().T@H@psi)**2)
+    return V[0,0]
+
+
+#Hamiltonian Params Traverse Field Ising model
+n_qubits=range(6,15)
+D=range(2,7,2)
+J=1
+h=0.5
+T=2
+
+#evolution parameters
+dt=0.1
+N=100 #max number of steps
+tmax=dt*N
+t=0 + np.arange(0, N+1) *dt
+
+for n in n_qubits:
+    #initial state
+    psi0np=np.zeros((2**n,1),dtype=complex)
+    psi0np[0]=1
+    psi_0=sp.csc_matrix(psi0np/np.linalg.norm(psi0np))
+    H,H_trot=ham.TFIM(J,h,n,T=T)
+    eig=sio.loadmat('eigsTFIM/N%d_J%1.1f_h%1.1f.mat'%(n,J,h))
+    psigs=eig['psi0']
+    for d in D:
+        print("Computing QITE for N=",n,"D=",d)
+        EACQ,psi_ACQ,indx = evol.ACQ(n,H,H_trot,d,psi_0,N,dt)
+        NQ=len(EACQ)
+        var_ACQ=np.zeros(NQ)
+        F_ACQ=np.zeros(NQ)
+        for i in range(NQ):
+            F_ACQ[i]=fidelity_pure(psigs,psi_ACQ[:,i].todense())
+            var_ACQ[i]=variance(psi_ACQ[:,i],H)
+        savedict = {
+            'N' : n_qubits,
+            'D' : D,
+            'T' : T,
+            'psi0' : psi_0,
+            'psiQITE' : psi_ACQ,
+            'J' : J,
+            'h' : h,
+            't' : t,
+            'E' : EACQ,
+            'var' : var_ACQ,
+            'F' : F_ACQ,
+            'indx' : indx
+        }
+        sio.savemat('runs_ACQ/ACQ_TFIM_J%1.1f_h%1.1f_N%d_D%d_T%d.mat'%(J,h,n,d,T), savedict)
+
+# %%
+psi_ACQ
+
+# %%
+EACQ.size
+
+# %%
+
+
+
