1e75179d733878f29c7a51acc65ae37b6a399457
Luca ion <lucaion781@gmail.com>
2026-05-20 15:27:06 +0200

    .

--- transpilation_and_DBQITE/transpile_qiskit.py
+++ transpilation_and_DBQITE/transpile_qiskit.py
@@ -148,10 +148,7 @@ def transpile_circuit_qite(n_qubits: int,
     tr_unitary = Operator(tr_qc_v)
     error = (tr_unitary.data - v_true) 
     trace_norm = np.linalg.norm(error)  #trace norm
-<<<<<<< HEAD
-    print(f'Error (trace norm) between transpiled circuit unitary and target unitary: {trace_norm:e}')
-=======
->>>>>>> origin/ACQ-Heisenber-DB
+    #print(f'Error (trace norm) between transpiled circuit unitary and target unitary: {trace_norm:e}')
 
     return tr_qc_v, v_true, trace_norm
 
@@ -231,10 +228,7 @@ def transpile_circuit_qite_adap(n_qubits: int,
     tr_unitary = Operator(tr_qc_u)
     error = (tr_unitary.data - u_true)
     trace_norm = np.linalg.norm(error) #trace norm
-<<<<<<< HEAD
-    print(f'Error (trace norm) between transpiled circuit unitary and target unitary: {trace_norm:e}')
-=======
->>>>>>> origin/ACQ-Heisenber-DB
+    #print(f'Error (trace norm) between transpiled circuit unitary and target unitary: {trace_norm:e}')
     #print(Operator(u_true).equiv(Operator(tr_qc_u)))
 
     return tr_qc_u, u_true, trace_norm
